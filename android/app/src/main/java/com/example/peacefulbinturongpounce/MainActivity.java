205d6a82998728b5bd0401812ec4ed2a704c4f25
Seyed Mohsen Mirhoseini <39451845+siedmohsen2@users.noreply.github.com>
2026-01-01 13:42:12 +0330

    Update MainActivity.java

--- android/app/src/main/java/com/example/peacefulbinturongpounce/MainActivity.java
+++ android/app/src/main/java/com/example/peacefulbinturongpounce/MainActivity.java
@@ -1,5 +1,12 @@
 package com.example.peacefulbinturongpounce;
 
-import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
